49014c19d3a1f5f4f1dcbbcc5d27c0c0281aad3f
intellectualDarknet <ladyblaumeux24@gmail.com>
2022-05-27 17:16:14 +0300

    refactor: change tags according to rule 3.1

--- app.js
+++ app.js
@@ -10,9 +10,9 @@
 
 var taskInput=document.getElementById("new-task");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder=document.getElementById("incompleteTasks");//ul of #incompleteTasks
+var incompleteTaskHolder=document.querySelector(".main__todo-list");//ul of #incompleteTasks
 var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
-
+console.log(incompleteTaskHolder);
 
 //New task list item
 var createNewTaskElement=function(taskString){
